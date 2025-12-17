51be10ff5e35d8065358833715f3c4dbeebf3402
Arwa Abdelhalim <arwa.abdelhalim@vodafone.com>
2025-12-17 18:04:38 +0200

    add functions for shared lib

--- vars/DAST.groovy
+++ vars/DAST.groovy
@@ -1,10 +1,35 @@
 def call (Map params = [:], String API){
-    sh """
-        cd /opt/zaproxy
-        ls -la
-        ./zap.sh -daemon -port 8080 -config api.key=${API} &
-        sleep 160
-        python3 ${env.WORKSPACE}/Dast-Scripts/Zap-scan.py --repo "${params.REPONAME}" || true
-        python3 ${env.WORKSPACE}/Dast-Scripts/Zap-scan.py --repo "${params.REPONAME}"
-    """
+     def resourceBase = 'Dast-Scripts'
+     def files = [
+        'Createpr.py',
+        'Zap-scan.py',
+        'extract-code.py',
+        'groovy-script',
+        'python.py'
+    ]
+
+    dir('Dast-Scripts') {
+        files.each { file ->
+            writeFile(
+                file: "./${file}",
+                text: libraryResource("${resourceBase}/${file}")
+            )
+        }
+
+        sh """
+            
+            cd /opt/zaproxy
+            ls -la
+            ./zap.sh -daemon -port 8080 -config api.key=${API} &
+            sleep 160
+            python3 ./Zap-scan.py --repo "${params.REPONAME}" || true
+            python3 ./Zap-scan.py --repo "${params.REPONAME}"
+        """
+    }   
+
 }
+
+
+
+
+   
