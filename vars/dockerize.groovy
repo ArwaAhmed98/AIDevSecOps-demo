4e3d4243a455a76eb39d4d3c07387e818b9b9821
Arwa Abdelhalim <arwa.abdelhalim@vodafone.com>
2025-12-17 20:01:17 +0200

    add functions for shared lib

--- vars/dockerize.groovy
+++ vars/dockerize.groovy
@@ -2,12 +2,12 @@ def call(){
 
     IMAGE_NAME="ar5678/aidevsecops:${BUILD_NUMBER}"
     sh """
+    ls -la /kaniko/.docker/
     # Kaniko command
     /kaniko/executor \
         --dockerfile Dockerfile \
         --context . \
         --destination=$IMAGE_NAME \
-        --docker-config /kaniko/.docker/ \
         --verbosity info
     """
 }
