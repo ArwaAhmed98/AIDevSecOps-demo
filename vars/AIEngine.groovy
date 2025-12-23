860b4b926fe38333adfbc7e72fda6cdde83b8223
Arwa Abdelhalim <arwa.abdelhalim@vodafone.com>
2025-12-23 18:20:23 +0200

    update init file

--- vars/AIEngine.groovy
+++ vars/AIEngine.groovy
@@ -8,11 +8,11 @@ def call(String AGENT = 'agents/default.yaml') {
         }
         parameters {
         string(name: 'ENV', defaultValue: 'demo', description: 'Env to deploy on')
-        string(name: 'REPO_URL', defaultValue: 'https://github.com/MostafaAnas/go-cli-todo', description: 'GitHub repository URL to scan')
+        string(name: 'REPO_URL', defaultValue: 'https://github.com/ArwaAhmed98/AIDevSecOps-app-demo', description: 'GitHub repository URL to scan')
         string(name: 'OLLAMA_MODEL', defaultValue: 'llama3.2:1b', description: 'Ollama model to use')
         string(name: 'OLLAMA_SERVER_URL', defaultValue: 'http://172.22.24.2:8080', description: 'Ollama server URL')
         string(name: 'OUTPUT_FILE', defaultValue: '', description: 'Output file name (optional)')
-        string(name: 'REPONAME', defaultValue: 'https://github.com/MostafaAnas/go-cli-todo', description: 'Repository name for DAST(e.g., org/repo or repo)')
+        string(name: 'REPONAME', defaultValue: 'https://github.com/ArwaAhmed98/AIDevSecOps-app-demo', description: 'Repository name for DAST(e.g., org/repo or repo)')
         booleanParam(name: 'RUN_RAISEPR', defaultValue: false, description: 'Tick to run RaisePR stage')
 
         }
